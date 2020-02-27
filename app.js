37770d165488b80923e45e7c61d000463de96794
Bruno Cardoso <br1@MBP-de-Bruno.cidade.citexvi.local>
2020-02-27 11:13:32 +0100

    first comit

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//cargar archivos rutas
+
+
+//middlewares
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//CORS
+
+//rutas
+app.get('/', (req, resp) => {
+    resp.status(200).send(
+        "<h1> Mi primera API con NodeJS</h1>"
+    );
+});
+
+app.get('/test', (req, resp) => {
+    resp.status(200).send({
+        message: "Probado mi API de NodeJS"
+    })
+});
+//exportar
+module.exports = app;
